f7158307c7f237d63f65cabdc35a64d403a796df
ommeghani <meghanio264@gmail.com>
2025-06-03 16:00:34 +0530

    change

---
+++ src/components/NewTransito.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+const NewTransiton = ({ transaction }) => {
+    const {deleteTransaction} = useContext(GlobalContext);
+const sign = transaction.amount < 0  ? '-': '+'
+  return (
+   <li className={transaction.amount < 0 ? 'minus': 'plus'} >
+   {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span> <button onClick={() => deleteTransaction} className='delete-btn'> X</button>
+   </li>
+  )
+}
+
+export default NewTransiton ; 
